b00e59a8815313e089f21ae69e1aeb13fe537d84
Kyung Jae Kim <kyung_jae.kim@spaceforce.mil>
2026-09-03 15:01:50 -0400

    Completed getting flights for squadron pilots in report generator

--- src/generate_squadron_report.py
+++ src/generate_squadron_report.py
@@ -19,34 +19,34 @@ def generate_squadron_report(squadron_code, output_file):
         output_file (str): Path to save the report
     """
     # TODO: PART 1 - Load the data files
-
     # Read and load pilots CSV:
     pilots_filepath = '../data/pilots.csv'
     pilots = rf.read_csv_file(pilots_filepath)
     print(pilots[0])
-
     # Read and load flight logs CSV:
     flight_logs_filepath = '../data/flight_logs.csv'
     flight_logs = rf.read_csv_file(flight_logs_filepath)
     print(flight_logs[0])
-
     # Read and load aircraft CSV:
     aircraft_filepath = '../data/aircraft.csv'
     aircraft = rf.read_csv_file(aircraft_filepath)
     print(aircraft[0])
 
     # TODO: PART 2 - Filter data for the specified squadron
-
     # Filter out pilots not belonging to specified squadron:
     squadron_pilots = rf.filter_by_field(pilots, 'squadron', squadron_code)
     print(squadron_pilots[0])
-
     # Filter out aircraft not belonging to specified squadron:
     squadron_aircraft = rf.filter_by_field(aircraft, 'squadron', squadron_code)
     print(squadron_aircraft[0])
 
     # TODO: PART 3 - Get flights for squadron pilots
-    pass
+    # Join pilots list with flight logs with pilot data by the shared pilot id column
+    joined_flight_logs = rf.join_data(flight_logs, pilots, 'pilot_id', 'pilot_id')
+    print(joined_flight_logs[0])
+    # Filter out joined flight logs that don't have matching squadron
+    squadron_flight_logs = rf.filter_by_field(joined_flight_logs, 'squadron', squadron_code)
+    print(squadron_flight_logs[0])
 
     # TODO: PART 4 - Calculate statistics
     pass
