f2fd69d3a75c0c289d1dd49e344f19d74122e32f
Kyung Jae Kim <kyung_jae.kim@spaceforce.mil>
2026-09-03 14:53:29 -0400

    Completed filtering for specified squadron in report generator

--- src/generate_squadron_report.py
+++ src/generate_squadron_report.py
@@ -20,25 +20,30 @@ def generate_squadron_report(squadron_code, output_file):
     """
     # TODO: PART 1 - Load the data files
 
-    # Set filepath, read the CSV, load data, print first line.
-
-    # Aircraft CSV:
-    aircraft_filepath = '../data/aircraft.csv'
-    aircraft = rf.read_csv_file(aircraft_filepath)
-    print(aircraft[0])
-
-    # Flight logs CSV:
-    flight_logs_filepath = '../data/flight_logs.csv'
-    flight_logs = rf.read_csv_file(flight_logs_filepath)
-    print(flight_logs[0])
-
-    # Pilots CSV:
+    # Read and load pilots CSV:
     pilots_filepath = '../data/pilots.csv'
     pilots = rf.read_csv_file(pilots_filepath)
     print(pilots[0])
 
+    # Read and load flight logs CSV:
+    flight_logs_filepath = '../data/flight_logs.csv'
+    flight_logs = rf.read_csv_file(flight_logs_filepath)
+    print(flight_logs[0])
+
+    # Read and load aircraft CSV:
+    aircraft_filepath = '../data/aircraft.csv'
+    aircraft = rf.read_csv_file(aircraft_filepath)
+    print(aircraft[0])
+
     # TODO: PART 2 - Filter data for the specified squadron
-    pass
+
+    # Filter out pilots not belonging to specified squadron:
+    squadron_pilots = rf.filter_by_field(pilots, 'squadron', squadron_code)
+    print(squadron_pilots[0])
+
+    # Filter out aircraft not belonging to specified squadron:
+    squadron_aircraft = rf.filter_by_field(aircraft, 'squadron', squadron_code)
+    print(squadron_aircraft[0])
 
     # TODO: PART 3 - Get flights for squadron pilots
     pass
@@ -58,7 +63,7 @@ if __name__ == '__main__':
     # TODO: Students will customize this to generate reports for different squadrons
     print("Generating squadron activity reports...")
 
-    print("\nImplement the function above, then uncomment to test!")
+    # print("\nImplement the function above, then uncomment to test!")
 
     # Example: Generate report for VFA-41 (Black Aces)
     generate_squadron_report('VFA-41', '../reports/vfa-41-report.txt')
